6ce54495020463919a9f952e47e1a8103eef9a2c
thequeenbea <turtlezdude@gmail.com>
2024-08-19 01:46:01 -0500

    omit visibility toggle in fade_in

--- scripts/ui/ui_circlefade.gd
+++ scripts/ui/ui_circlefade.gd
@@ -8,7 +8,6 @@ func _ready() -> void:
 	await get_tree().create_timer(0.5).timeout
 	fade_in()
 func fade_in() -> void:
-	visible = true
 	var tweenshader: Callable = func _tweenshader(value: float) -> void:
 		material.set_shader_parameter("size", value)
 	var tween: Tween = create_tween()
@@ -24,4 +23,3 @@ func fade_out() -> void:
 	tween.tween_method(tweenshader, 300.0, 0.0, 1.0)
 	await tween.finished
 	await get_tree().create_timer(0.2).timeout
-	visible = false
